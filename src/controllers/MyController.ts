0f5215b2e79dd6a46175dcf05f2f5b6520750ce5
santi <santialbo@gmail.com>
2013-06-22 01:41:53 +0200

    Show sample Controller usage in sample View

--- src/controllers/MyController.ts
+++ src/controllers/MyController.ts
@@ -6,7 +6,7 @@ module app.controllers {
 
     export class MyController implements IController {
         constructor (private $scope) {
-            $scope.things = [1,2,3,4]
+            $scope.message = 'Hello world!';
         }
     }
 
